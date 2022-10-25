9bad17e36867679f9ed7b956c1076a3100d82b49
Marco Ricci <mr.ricci79@gmail.com>
2022-10-25 17:01:04 +0100

    customer controller done

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
 # TODO: require relevant files to bootstrap the app.
 # Then you can test your program with:
 #   ruby app.rb
+
+
+require_relative './app/controllers/meals_controller.rb'
+require_relative './app/repositories/customer_repository.rb'
+
 require_relative './app/controllers/meals_controller.rb'
 require_relative './app/repositories/meal_repository.rb'
+
 require_relative 'router'
 
-csv_file = File.join(__dir__, './data/meals.csv')
-meal_repo = MealRepository.new(csv_file)
-meal_controller = MealsController.new
+customers_csv_file = File.join(__dir__, './data/customers.csv')
+customer_repository = CustomerRepository.new(customers_csv_file)
+customers_controller = CustomersController.new(customer_repository)
+
+meals_csv_file = File.join(__dir__, './data/meals.csv')
+meal_repository = MealRepository.new(meals_csv_file)
+meal_controller = MealsController.new(meal_repository)
+
 router = Router.new
 router.run
